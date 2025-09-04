806435f42cbd60995006ca6b67fbe0fd35bc9d88
SebastianHpe <sebastianhoppe@hotmail.de>
2025-09-04 13:41:45 +0200

    feat: Add contexts

--- flaskblog.py
+++ flaskblog.py
@@ -2,14 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Sebastian Hoppe",
+        "title": "Blog Post 1",
+        "content": "First Post Content",
+        "date_posted": "September 04, 2025"
+    },
+    {
+        "author": "Luisa Harmening",
+        "title": "Blog Post 2",
+        "content": "Second Post Content",
+        "date_posted": "September 04, 2025"
+    },
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 if __name__ == "__main__":
     app.run(debug=True)
