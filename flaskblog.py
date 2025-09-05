a63f10556d47d66e4ab67e32a23303d3c8201945
SebastianHpe <sebastianhoppe@hotmail.de>
2025-09-05 10:34:23 +0200

    feat: Add registration route and template

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "31ae41c06b443067a21ed022c9736d29"
 
 posts = [
     {
@@ -25,6 +28,14 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
