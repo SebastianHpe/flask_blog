edae20c80dfc2da9ece961301b25c062c86cd772
SebastianHpe <sebastianhoppe@hotmail.de>
2025-09-08 15:15:32 +0200

    build: Add bcrypt

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 def create_app(config_class="config.Config") -> Flask:
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     db.init_app(app)
-
+    bcrypt.init_app(app)
+    
     from . import models  # noqa: F401  # Ensure models are registered
 
     from .routes import main
