5b202a6440f9c1a3f0e33da9464b57f2d48dab3e
SebastianHpe <sebastianhoppe@hotmail.de>
2025-09-09 09:07:05 +0200

    feat: Add login and logout functionality

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, url_for, flash, redirect
+from flask_login import login_user, logout_user, current_user
 from .forms import RegistrationForm, LoginForm
 from . import db
 from .models import User
@@ -8,6 +9,8 @@ main = Blueprint("main", __name__)
 
 @main.route("/register", methods=["GET", "POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("main.home"))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -22,12 +25,23 @@ def register():
 
 @main.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("main.home"))
     form = LoginForm()
     if form.validate_on_submit():
-        flash("You have been logged in!", "success")
-        return redirect(url_for("main.register"))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember.data)
+            flash("You have been logged in!", "success")
+            return redirect(url_for("main.home"))
+        else:
+            flash("Login unsuccessful. Please check email and password.", "danger")
     return render_template("login.html", title="Login", form=form)
 
+@main.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("main.home"))
 
 @main.route("/")
 @main.route("/home")
